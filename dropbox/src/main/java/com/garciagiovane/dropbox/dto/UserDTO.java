992e6beb83059a0e5475c1e9816e5a788dcec65e
Giovane Garcia <giovane.garcia09@gmail.com>
2019-09-11 15:13:30 -0300

    add builder annotation

--- dropbox/src/main/java/com/garciagiovane/dropbox/dto/UserDTO.java
+++ dropbox/src/main/java/com/garciagiovane/dropbox/dto/UserDTO.java
@@ -1,9 +1,11 @@
 package com.garciagiovane.dropbox.dto;
 
 import com.garciagiovane.dropbox.model.User;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class UserDTO {
     private String name;
     private String email;
