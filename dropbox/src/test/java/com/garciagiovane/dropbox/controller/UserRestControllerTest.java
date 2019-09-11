666e36f9f7f6f96e4e112c887e3ce7c070849c41
Giovane Garcia <giovane.garcia09@gmail.com>
2019-09-11 16:12:24 -0300

    add teste to UserRestController

--- dropbox/src/test/java/com/garciagiovane/dropbox/controller/UserRestControllerTest.java
+++ dropbox/src/test/java/com/garciagiovane/dropbox/controller/UserRestControllerTest.java
@@ -1,25 +1,29 @@
 package com.garciagiovane.dropbox.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.garciagiovane.dropbox.controller.service.UserService;
-import static org.junit.Assert.assertThat;
 
 import com.garciagiovane.dropbox.dto.UserDTO;
+import com.garciagiovane.dropbox.exception.NoFilesFoundException;
+import com.garciagiovane.dropbox.exception.UserNotFoundException;
+import com.garciagiovane.dropbox.model.ShareEntity;
 import com.garciagiovane.dropbox.model.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import static org.mockito.BDDMockito.given;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -34,10 +38,46 @@ public class UserRestControllerTest {
 
     @Test
     public void testCreateUser() throws Exception {
-        UserDTO userDTO = new UserDTO();
-        userDTO.setName("Victoria");
-        userDTO.setEmail("vic@mail.com");
-        MvcResult result = this.mockMvc.perform(post("/users").contentType(MediaType.APPLICATION_JSON).content(new ObjectMapper().writeValueAsString(userDTO))).andExpect(status().isOk()).andReturn();
-        System.out.println(result.getResponse().getContentAsString());
+        given(this.userService.createUser(UserDTO.builder().name("victória").email("vic@mail.com").build()))
+                .willReturn(User.builder().id("someid").name("test").email("test@mail.com").build());
+
+        this.mockMvc.perform(post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper()
+                        .writeValueAsString(UserDTO
+                                .builder()
+                                .name("victória")
+                                .email("vic@mail.com").build())))
+                .andExpect(status().isOk()).andExpect(jsonPath("$.id").value("someid"));
+    }
+
+    @Test
+    public void getUserById() throws Exception {
+        given(this.userService.getUserById("someid")).willReturn(User.builder().id("someid").name("test").email("test@mail.com").build());
+        this.mockMvc.perform(get("/users/someid")).andExpect(jsonPath("$.name").value("test"));
+    }
+
+    @Test
+    public void updateUserById() throws Exception {
+        given(this.userService.updateUserById(UserDTO.builder().name("giovane").email("giovane@.mail.com").build(), "someid"))
+                .willReturn(User.builder().id("someid").name("giovane-alterado").email("giovane@mail.com").build());
+        this.mockMvc.perform(put("/users/someid").contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(UserDTO.builder().name("giovane").email("giovane@.mail.com").build())))
+                .andExpect(jsonPath("$.name").value("giovane-alterado"));
+    }
+
+    @Test
+    public void deleteUserById() throws Exception {
+        given(this.userService.deleteUserById("someid")).willReturn(ResponseEntity.noContent().build());
+        this.mockMvc.perform(delete("/users/someid")).andExpect(status().isNoContent());
+    }
+
+    @Test
+    public void createShare() throws Exception {
+        given(this.userService.shareFileById("someid", new ShareEntity("fileid", "userToShareWithId")))
+                .willReturn(ResponseEntity.noContent().build());
+        this.mockMvc.perform(post("/users/someid")
+                .contentType(MediaType.APPLICATION_JSON).content(new ObjectMapper()
+                        .writeValueAsString(new ShareEntity("fileid", "userToShareWithId")))).andExpect(status().isNoContent());
     }
 }
