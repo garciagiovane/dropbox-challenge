2a2bc9bc2eafb072fc4a8791b274d7c3cb564c5c
Giovane Garcia <giovane.garcia09@gmail.com>
2019-09-19 15:18:53 -0300

    add description to parameters and response status code on swagger

--- dropbox/src/main/java/com/garciagiovane/dropbox/controller/FileRestController.java
+++ dropbox/src/main/java/com/garciagiovane/dropbox/controller/FileRestController.java
@@ -8,6 +8,9 @@ import com.garciagiovane.dropbox.exception.NoFilesFoundException;
 import com.garciagiovane.dropbox.exception.UserNotFoundException;
 import com.garciagiovane.dropbox.model.UserFile;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -30,26 +33,43 @@ public class FileRestController {
     }
 
     @ApiOperation(value = "Return the list of all files saved on the database")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "return a list all files in the database")
+    })
     @GetMapping(value = "/files", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Page<UserFile> getAllFiles(Pageable pageable) {
         return fileService.getAllFiles(pageable);
     }
 
     @ApiOperation(value = "return a list of files the user has in the FTP server according to the user id, you can pass name parameter to filter by name or pass page and size parameters to paginate, can also pass both")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "return a list of all files from the user"),
+            @ApiResponse(code = 404, message = "exception occurred, user not found or files not found")
+    })
     @GetMapping(value = "/{userId}/files", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public Page<UserFileDTO> getFilesByUserId(@PathVariable String userId, @RequestParam Optional<String> name, Pageable pageable) throws UserNotFoundException, NoFilesFoundException {
+    public Page<UserFileDTO> getFilesByUserId(@ApiParam(value = "user id", required = true) @PathVariable String userId, @ApiParam(value = "file name") @RequestParam Optional<String> name, Pageable pageable) throws UserNotFoundException, NoFilesFoundException {
         return fileService.getFilesByName(userId, name, pageable);
     }
 
     @ApiOperation(value = "Posting a file at the file parameter and passing the user id from the owner you will create a doc file this user and will receive a json representation for that file that was stored in an database, you need to send a multipartfile at the 'file' parameter")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "return JSON representing the file created"),
+            @ApiResponse(code = 404, message = "exception occurred, user not found"),
+            @ApiResponse(code = 500, message = "exception occurred, connection to ftp refused")
+    })
     @PostMapping(value = "/{userId}/files", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public UserFile saveFile(@PathVariable String userId, @RequestParam MultipartFile file) throws UserNotFoundException, IOException, ConnectionRefusedException {
+    public UserFile saveFile(@ApiParam(value = "user id") @PathVariable String userId, @ApiParam(value = "multipart file") @RequestParam MultipartFile file) throws UserNotFoundException, IOException, ConnectionRefusedException {
         return fileService.saveFile(userId, file);
     }
 
     @ApiOperation(value = "Do a delete request passing the file owner id and the file id you will delete the file from the ftp server and database")
+    @ApiResponses(value = {
+            @ApiResponse(code = 204, message = "no message will be returned"),
+            @ApiResponse(code = 404, message = "exception occurred, user not found, file not found or ftp directory not found"),
+            @ApiResponse(code = 500, message = "exception occurred, access to ftp refused"),
+    })
     @DeleteMapping("/{userId}/files/{fileId}")
-    public ResponseEntity deleteFileById(@PathVariable String userId, @PathVariable String fileId) throws UserNotFoundException, IOException, NoFilesFoundException, ConnectionRefusedException, DirectoryNotFoundException {
+    public ResponseEntity deleteFileById(@ApiParam(value = "owner's file id", required = true) @PathVariable String userId, @ApiParam(value = "file id", required = true) @PathVariable String fileId) throws UserNotFoundException, IOException, NoFilesFoundException, ConnectionRefusedException, DirectoryNotFoundException {
         return fileService.deleteFileById(userId, fileId);
     }
 }
--- dropbox/src/main/java/com/garciagiovane/dropbox/controller/UserRestController.java
+++ dropbox/src/main/java/com/garciagiovane/dropbox/controller/UserRestController.java
@@ -5,8 +5,7 @@ import com.garciagiovane.dropbox.dto.UserDTO;
 import com.garciagiovane.dropbox.exception.*;
 import com.garciagiovane.dropbox.model.ShareEntity;
 import com.garciagiovane.dropbox.model.User;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -28,38 +27,61 @@ public class UserRestController {
     }
 
     @ApiOperation(value = "Get all users from the database, you can define page and size variable in the URI to paginate")
+    @ApiResponse(code = 200, message = "return a list of all users in the database")
     @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Page<User> getAllUsers(Pageable pageable) {
         return userService.findAll(pageable);
     }
 
     @ApiOperation(value = "Receive a JSON with name and email fields to save it on the database and return the created object")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "return an object representing the user saved"),
+            @ApiResponse(code = 500, message = "exception occurred, user already registered, email is already in use")
+    })
     @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public User createUser(@RequestBody UserDTO userDTO) throws UserAlreadyRegisteredException {
+    public User createUser(@ApiParam(value = "JSON with name and email") @RequestBody UserDTO userDTO) throws UserAlreadyRegisteredException {
         return userService.createUser(userDTO);
     }
 
     @ApiOperation(value = "Return all information from a user according the id passed or throw an exception for user not found")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "return a user of the database"),
+            @ApiResponse(code = 404, message = "exception occurred, user was not found")
+    })
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public User getUserById(@PathVariable String id) throws UserNotFoundException {
+    public User getUserById(@ApiParam(value = "user id", required = true) @PathVariable String id) throws UserNotFoundException {
         return userService.getUserById(id);
     }
 
     @ApiOperation(value = "Receive a JSON with name and email fields and update a user saved or throw an exception for user not found")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "return the user updated"),
+            @ApiResponse(code = 404, message = "exception occurred, user was not found"),
+            @ApiResponse(code = 500, message = "exception occurred, user is already registered, email changed is already in use"),
+    })
     @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public User updateUserById(@RequestBody UserDTO userDTO, @PathVariable String id) throws UserNotFoundException, UserAlreadyRegisteredException {
+    public User updateUserById(@ApiParam(value = "JSON with user name and email") @RequestBody UserDTO userDTO, @ApiParam(value = "user id", required = true) @PathVariable String id) throws UserNotFoundException, UserAlreadyRegisteredException {
         return userService.updateUserById(userDTO, id);
     }
 
     @ApiOperation(value = "Delete a user according the id passed or throw an exception for user not found")
+    @ApiResponses( value = {
+            @ApiResponse(code = 204, message = "no message returned"),
+            @ApiResponse(code = 404, message = "exception occurred, user was not found"),
+            @ApiResponse(code = 500, message = "exception occurred, access to ftp was denied or user directory in the ftp server doesn't exists")
+    })
     @DeleteMapping("/{id}")
-    public ResponseEntity deleteUserById(@PathVariable String id) throws UserNotFoundException, ConnectionRefusedException, NoFilesFoundException, DirectoryNotFoundException, IOException {
+    public ResponseEntity deleteUserById(@ApiParam(value = "user id", required = true) @PathVariable String id) throws UserNotFoundException, ConnectionRefusedException, NoFilesFoundException, DirectoryNotFoundException, IOException {
         return userService.deleteUserById(id);
     }
 
     @ApiOperation(value = "Receive a JSON with file id (previously saved) and a user id (also previously saved), then share the file with this user or throw an exception for user not found or file not found")
+    @ApiResponses(value = {
+            @ApiResponse(code = 204, message = "no message returned"),
+            @ApiResponse(code = 404, message = "exception occurred, user was not found or no files was not found")
+    })
     @PostMapping("/{ownerId}")
-    public ResponseEntity createShare(@PathVariable String ownerId, @RequestBody ShareEntity shareEntity) throws UserNotFoundException, NoFilesFoundException {
+    public ResponseEntity createShare(@ApiParam(value = "owner's file id", required = true) @PathVariable String ownerId, @ApiParam(value = "JSON with file id and user id that will receive the file shared", required = true) @RequestBody ShareEntity shareEntity) throws UserNotFoundException, NoFilesFoundException {
         return userService.shareFileById(ownerId, shareEntity);
     }
 }
